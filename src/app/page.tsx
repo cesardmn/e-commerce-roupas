a898b51a22d1d0c8a1bf5d026e70832777940209
Sergio <sergiohrodriguess@gmail.com>
2023-05-19 16:47:11 -0300

    tela inicial concluded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Menu />
-      {/* <Conteudo /> */}
+      <Conteudo />
     </>
   )
 }
--- src/components/Conteudo/index.tsx
+++ src/components/Conteudo/index.tsx
@@ -1,16 +1,93 @@
 import React from 'react'
 import { styled } from 'styled-components'
+import json from '../../json/itens.json'
+import Image from 'next/image'
 
 const ConteudoContainer = styled.main`
-    display: grid;
-    grid-template-columns: 20% 80%;
+  margin-top: 30%;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  padding: 3rem 0;
+  `
+const MaisVendidosContainer = styled.section`
+  h2{
+    font-size: 1.3rem;
+    text-align: center;
+  }
+  ul{
+    overflow-x: scroll;
+    margin-top: 1rem;
+    display: flex;
+    gap: 1rem;
+    li{
+      width: 30%;
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+      button{
+        width: 100%;
+      }
+    }
+  }
+`
+
+const AdicionadosRecentementeContainer = styled.section`
+  h2{
+    font-size: 1.3rem;
+    text-align: center;
+  }
+  ul{
+    overflow-x: scroll;
+    margin-top: 1rem;
+    display: flex;
+    gap: 1rem;
+    li{
+      width: 30%;
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+      button{
+        width: 100%;
+      }
+    }
+  }
 `
 
 export default function Conteudo() {
+  const produtos = json.roupas
+  const mapProdutos = produtos.map(item => item)
+
+  const adicionadosRecentemente = mapProdutos.splice(mapProdutos.length -10, 10)
+
   return (
     <ConteudoContainer>
-        <section>Filtros</section>
-        <section>Conteudo</section>
+          <MaisVendidosContainer>
+            <h2>Mais vendidos</h2>
+            <ul>
+            {produtos.map((prod, index) => (
+              <li key={index}>
+                <Image src={`/imagens/imagem${prod.id}.png`} width={100} height={100} alt={prod.titulo} />
+                <button>Mais detalhes</button>
+              </li>
+            ))}
+              <li>
+
+              </li>
+            </ul>
+          </MaisVendidosContainer>
+
+          <AdicionadosRecentementeContainer>
+            <h2>Adicionados recentemente</h2>
+            <ul>
+              {adicionadosRecentemente.map((prod, index) => (
+                <li key={index}>
+                  <Image src={`/imagens/imagem${prod.id}.png`} width={100} height={100} alt={prod.titulo} />
+                  <button>Mais detalhes</button>
+                </li>
+              ))}
+            </ul>
+          </AdicionadosRecentementeContainer>
     </ConteudoContainer>
   )
 }
